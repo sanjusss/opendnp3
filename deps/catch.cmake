da4434a6aef5e33b3c7efbbd0b7a1a79158c3d1d
sanjusss <sanjusss@qq.com>
2025-04-24 13:26:28 +0800

    Use find_package instead of direct download. 使用find_package代替直接下载。

--- deps/catch.cmake
+++ deps/catch.cmake
@@ -1,20 +1,5 @@
-include(FetchContent)
-
-FetchContent_Declare(
-    catch
-    URL                 https://github.com/catchorg/Catch2/releases/download/v2.11.3/catch.hpp
-    URL_HASH            SHA1=C127EBB7A4F65C6CEFF7587C8EF18F84A74D6C15
-    DOWNLOAD_NO_EXTRACT TRUE
-    DOWNLOAD_DIR        ${CMAKE_CURRENT_BINARY_DIR}/catch-src
-)
-
-FetchContent_GetProperties(catch)
-if(NOT catch_POPULATED)
-    FetchContent_Populate(catch)
-
-    find_package(Threads)
-
-    add_library(catch INTERFACE)
-    target_include_directories(catch INTERFACE ${CMAKE_CURRENT_BINARY_DIR}/catch-src)
-    target_compile_features(catch INTERFACE cxx_std_11)
-endif()
+find_package(Catch2 REQUIRED)
+add_library(catch ALIAS Catch2::Catch2)
+find_package(Threads)
+target_link_libraries(Catch2::Catch2 INTERFACE Threads::Threads)
+target_compile_features(Catch2::Catch2 INTERFACE cxx_std_11)
